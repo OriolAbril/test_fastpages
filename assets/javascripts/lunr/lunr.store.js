e82cded765ee2b7cf400ce95a1e15d36bede363e
OriolAbril <OriolAbril@users.noreply.github.com>
2020-05-02 03:48:20 +0000

    deploy: 08d7becbd7cd5f5f48bba448d899b9613a913eaf

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Introduction to ArviZ: InferenceData objects",
+        "excerpt":"About This notebook is an introduction to InferenceData objects and their role in ArviZ. It aims to be as hands-on as possible, using examples to cover different use cases of the data in each group, examples on how to combine several InferenceData objects and so on. The key idea behind...","categories": ["arviz","InferenceData","data"],
+        "tags": [],
+        "url": "https://oriolabril.github.io/test_fastpages/arviz/inferencedata/data/2020/04/30/InferenceData.html"
+      },{
+        "title": "ArviZ customization: `plot_trace`",
+        "excerpt":"                    ","categories": ["arviz","visualization","plotting"],
+        "tags": [],
+        "url": "https://oriolabril.github.io/test_fastpages/arviz/visualization/plotting/2020/04/30/plot-trace.html"
+      },{
+        "title": "ArviZ customization with rcParams",
+        "excerpt":"Comments are not enabled for the blog, to inquiry further about the contents of the post, ask on ArviZ Issues or PyMC Discourse About ArviZ not only builds on top of matplotlib's rcParams but also adds its own rcParams instance to handle specific settings. This post will only graze matplotlib's...","categories": ["arviz","customization","rcparams"],
+        "tags": [],
+        "url": "https://oriolabril.github.io/test_fastpages/arviz/customization/rcparams/2020/04/30/rcParams.html"
+      }]
